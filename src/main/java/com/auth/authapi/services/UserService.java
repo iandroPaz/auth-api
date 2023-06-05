2ce4f08529085989fc976f56d7619fba47b2803e
Gisele <49593120+iandroPaz@users.noreply.github.com>
2023-06-05 14:37:26 -0300

    fix-update-imports

--- src/main/java/com/auth/authapi/services/UserService.java
+++ src/main/java/com/auth/authapi/services/UserService.java
@@ -4,12 +4,11 @@ import com.auth.authapi.models.User;
 
 import org.springframework.stereotype.Service;
 
-import com.auth.authapi.UserMapper;
 import com.auth.authapi.dtos.UserDTO;
+import com.auth.authapi.mappers.UserMapper;
 
 @Service
 public class UserService {
-	
 	public UserDTO create (UserDTO userDto) {
 		User user = new User(userDto.getLogin(), userDto.getPassword(), userDto.getStatus());
 		return UserMapper.map(user);
